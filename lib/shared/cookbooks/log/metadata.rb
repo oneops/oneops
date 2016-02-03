c939d8fb433ddcef1a1164180a389a56263370ec
mike schwankl <mschwan@walmart.com>
2016-02-03 10:46:01 -0700

    remove readme.md dependency in metadata

--- lib/shared/cookbooks/log/metadata.rb
+++ lib/shared/cookbooks/log/metadata.rb
@@ -1,6 +1,5 @@
 name             "Log"
 description      "Log"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1"
 maintainer       "OneOps"
 maintainer_email "support@oneops.com"
--- lib/shared/cookbooks/simple_iptables/metadata.rb
+++ lib/shared/cookbooks/simple_iptables/metadata.rb
@@ -2,7 +2,6 @@ maintainer       "Dan Crosta"
 maintainer_email "dcrosta@late.am"
 license          "BSD"
 description      "Simple LWRP and recipe for managing iptables rules"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.3.0"
 name             "simple_iptables"
 
