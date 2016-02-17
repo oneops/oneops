fbd1e153318a110059a693956e68079d2fb23698
Bhaskar Annamalai - bannama <bannama@L-SB8X2BPG8W-M.local>
2016-02-16 18:17:22 -0800

    send messages to search async

---
+++ src/main/java/com/oneops/cms/transmitter/EventPublisher.java
@@ -0,0 +1,129 @@
+/*******************************************************************************
+ *  
+ *   Copyright 2015 Walmart, Inc.
+ *  
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *  
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *  
+ *******************************************************************************/
+package com.oneops.cms.transmitter;
+
+import javax.jms.JMSException;
+
+import com.oneops.antenna.domain.NotificationMessage;
+import com.oneops.antenna.domain.NotificationSeverity;
+import com.oneops.antenna.domain.NotificationType;
+import com.oneops.cms.cm.domain.CmsCI;
+import com.oneops.cms.cm.ops.domain.CmsOpsProcedure;
+import com.oneops.cms.dj.domain.CmsDeployment;
+import com.oneops.cms.transmitter.domain.CMSEvent;
+import com.oneops.cms.transmitter.domain.EventSource;
+import com.oneops.util.ReliableExecutor;
+
+public class EventPublisher {
+
+	private CmsPublisher cmsPublisher;
+	
+	private SearchPublisher searchPublisher;
+	
+    private ReliableExecutor<NotificationMessage> antennaClient;
+
+    private NotificationConfigurator notificationConfig;
+
+    public void publishControllerEvents(CMSEvent event) throws JMSException {
+    	notifyEvent(event);
+    	cmsPublisher.publishMessage(event);
+    	searchPublisher.publishMessage(event);
+    }
+    
+    public void publishCIEvents(CMSEvent event) throws JMSException {
+    	notifyEvent(event);
+    	searchPublisher.publishMessage(event);
+    }
+    
+    public void notifyEvent(CMSEvent event) throws JMSException {
+
+        NotificationMessage notify = createNotificationMessage(event);
+        if(notify != null) {
+            antennaClient.executeAsync( notify );
+        }
+    }
+    
+    private NotificationMessage createNotificationMessage(CMSEvent event) {
+        //no config - no notification
+        if(!notificationConfig.isConfigured()) {
+            return null;
+        }
+        NotificationMessage notify = new NotificationMessage();
+        notify.setTimestamp(System.currentTimeMillis());
+        String s = event.getHeaders().get("source");
+        String c = event.getHeaders().get("clazzName");
+        EventSource source = EventSource.toEventSource(s);
+        NotificationType type = null;
+        NotificationRule rule = notificationConfig.getRule(source, c);
+        if(rule == null) {
+            return null;
+        }
+        if(source.equals(EventSource.deployment)) {
+            type = NotificationType.deployment;
+            CmsDeployment dp = (CmsDeployment)event.getPayload();
+            notify.setSubject("Deployment" );
+            notify.setText("Deployment: "+dp.getDescription());
+        } else if(source.equals(EventSource.opsprocedure)) {
+            type = NotificationType.procedure;
+            CmsOpsProcedure proc = (CmsOpsProcedure)event.getPayload();
+            notify.setSubject("Procedure");
+            notify.setText("Procedure: " + proc.getProcedureName() + " is " + proc.getProcedureState());
+        } else if(source.equals(EventSource.cm_ci)) {
+            CmsCI ci = (CmsCI)event.getPayload();
+            // todo: selection by Ci name
+            type = NotificationType.ci;
+            notify.setCmsId(ci.getCiId());
+            notify.setNsPath(ci.getNsPath());
+            notify.setSubject("ci:" + ci.getCiName() );
+            notify.setText(ci.getNsPath() + "ci:" + ci.getCiName() );
+        } else if(source.equals(EventSource.cm_ci_rel)) {
+            type = NotificationType.ci;
+            notify.setSubject("ci_rel:" );
+            notify.setText("Ci Relation");
+        }
+        if(rule.getSubject() != null) {
+            notify.setSubject(rule.getSubject());
+        }
+        if(rule.getMessage() != null) {
+            notify.setText(rule.getMessage());
+        }
+        notify.setSource(s);
+        notify.setType( type );
+        //todo: severity selection is ?
+        notify.setSeverity(NotificationSeverity.info);
+
+        return notify;
+    }
+    
+    public void setCmsPublisher(CmsPublisher cmsPublisher) {
+		this.cmsPublisher = cmsPublisher;
+	}
+
+	public void setSearchPublisher(SearchPublisher searchPublisher) {
+		this.searchPublisher = searchPublisher;
+	}
+
+	public void setAntennaClient(ReliableExecutor<NotificationMessage> antennaClient) {
+        this.antennaClient = antennaClient;
+    }
+
+    public void setNotificationConfig(NotificationConfigurator notificationConfig) {
+        this.notificationConfig = notificationConfig;
+    }
+	
+}
---
+++ src/test/java/com/oneops/cms/transmitter/JMSConsumer.java
@@ -0,0 +1,157 @@
+/*******************************************************************************
+ *  
+ *   Copyright 2015 Walmart, Inc.
+ *  
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *  
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *  
+ *******************************************************************************/
+package com.oneops.cms.transmitter;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.jms.Connection;
+import javax.jms.Destination;
+import javax.jms.ExceptionListener;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import com.oneops.util.MessageData;
+
+public class JMSConsumer implements ExceptionListener {
+	
+	private ActiveMQConnectionFactory connectionFactory;
+	private String destinationName;
+	private String destinationType;
+	
+	private MessageConsumer consumer;
+	private Session session;
+	private Connection connection;
+	
+	private AtomicInteger counter = new AtomicInteger(0);
+	
+	private LinkedList<MessageData> messages;
+	private AtomicBoolean isRecording = new AtomicBoolean(false);
+	
+	public void init() {
+		new Thread(() -> startConsumer()).start();
+	}
+	
+	public void startConsumer() {
+		try {
+			connection = connectionFactory.createConnection();
+			connection.start();
+			session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			Destination destination = null;
+			if ("topic".equalsIgnoreCase(destinationType)) {
+				destination = session.createTopic(destinationName);	
+			}
+			else {
+				destination = session.createQueue(destinationName);
+			}
+			
+			consumer = session.createConsumer(destination);
+			while (true) {
+				Message message = consumer.receive();
+
+				if (message instanceof TextMessage) {
+					TextMessage textMessage = (TextMessage) message;
+					String text = textMessage.getText();
+					System.out.println("Received: " + text);
+					counter.incrementAndGet();
+					if (isRecording.get()) {
+						addData(message, text);
+					}
+				} 
+			}
+
+		} catch (Exception e) {
+			//e.printStackTrace();
+		} finally {
+			terminate();
+		}
+	}
+	
+	private void addData(Message message, String text) throws JMSException {
+		MessageData data = new MessageData();
+		data.setPayload(text);
+		Map<String, String> headers = new HashMap<String, String>();
+		Enumeration<String> names = message.getPropertyNames();
+		while (names.hasMoreElements()) {
+			String name = names.nextElement();
+			String value = message.getStringProperty(name);
+			headers.put(name, value);
+		}
+		data.setHeaders(headers);
+		messages.add(data);
+	}
+	
+	public void startRecording() {
+		messages = new LinkedList<MessageData>();
+		isRecording.getAndSet(true);
+	}
+	
+	public void stopRecording() {
+		messages = null;
+		isRecording.getAndSet(false);
+	}
+	
+	public void terminate() {
+		try {
+			consumer.close();
+			session.close();
+			connection.close();
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void resetCounter() {
+		counter.getAndSet(0);
+	}
+
+	@Override
+	public void onException(JMSException exception) {
+		exception.printStackTrace();
+	}
+
+	public int getCounter() {
+		return counter.get();
+	}
+
+	public void setConnectionFactory(ActiveMQConnectionFactory connectionFactory) {
+		this.connectionFactory = connectionFactory;
+	}
+
+	public void setDestinationName(String destinationName) {
+		this.destinationName = destinationName;
+	}
+
+	public void setDestinationType(String destinationType) {
+		this.destinationType = destinationType;
+	}
+
+	public LinkedList<MessageData> getMessages() {
+		return messages;
+	}
+
+}
