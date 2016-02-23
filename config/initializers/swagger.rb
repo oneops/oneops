5dc51671afa70f6a749f3d00eb6f44c5735c3066
Lev Khusid <lkhusid@gmail.com>
2016-02-23 13:08:54 -0500

    Swagger config change for base path.

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -34,7 +34,8 @@ Swagger::Docs::Config.register_apis(
       # the output location where your .json files are written to
       :api_file_path      => 'public/apidocs',
       # the URL base path to your API
-      :base_path          => Rails.env.shared? || Rails.env.development? ? 'http://localhost:3000' : '/',
+      # :base_path          => Rails.env.shared? || Rails.env.development? ? 'http://localhost:3000' : '/',
+      :base_path          => "#{Settings.protocol}://#{Settings.host}:#{Settings.port}",
       # if you want to delete all .json files at each generation
       :clean_directory    => true,
       # add custom attributes to api-docs
