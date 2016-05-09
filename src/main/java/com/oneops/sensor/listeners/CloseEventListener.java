2059750c7bb282cea2af677327fa446a6e0f4911
Bhaskar Annamalai - bannama <bannama@L-SB8X2BPG8W-M.local>
2016-05-09 12:34:13 -0700

    removed orphan delete from close event listener

--- src/main/java/com/oneops/sensor/listeners/CloseEventListener.java
+++ src/main/java/com/oneops/sensor/listeners/CloseEventListener.java
@@ -112,7 +112,6 @@ public class CloseEventListener implements UpdateListener {
                 }
                 opsEventPub.publishCiStateMessage(ciEvent);
                 publishedMessage = true;
-                opsEventDao.removeOrphanEvent(event.getCiId(), event.getManifestId(), event.getName());
             }
             else {
             	if (orphanEventEnabled) {
