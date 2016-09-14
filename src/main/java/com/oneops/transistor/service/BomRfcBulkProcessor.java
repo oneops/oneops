61978a19f4eeee6707b3e4c6fa2c6fc53ecda5d6
jayeshpise <jayesh.pise@gmail.com>
2016-09-14 10:29:51 -0700

    removed unused import

--- src/main/java/com/oneops/transistor/service/BomRfcBulkProcessor.java
+++ src/main/java/com/oneops/transistor/service/BomRfcBulkProcessor.java
@@ -28,7 +28,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 
-import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
 import com.google.gson.Gson;
