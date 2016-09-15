9d9c14b4571abcaefe893bce86ab3f40bc58daab
Oleg Kornev <kornev@gmail.com>
2016-09-14 20:56:15 -0400

    imports

--- src/main/java/com/oneops/cms/ws/rest/CmRestController.java
+++ src/main/java/com/oneops/cms/ws/rest/CmRestController.java
@@ -20,11 +20,9 @@ package com.oneops.cms.ws.rest;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletResponse;
 
