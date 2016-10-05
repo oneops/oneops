a62ed429cd619a29e139454e92d395f37d8aa83b
Raj Anand <rajanand@walmartlabs.com>
2016-10-05 11:32:33 -0700

    Update batch-script.sh

--- cost/batch-script.sh
+++ cost/batch-script.sh
@@ -4,6 +4,5 @@ n=0
    do
       ./cost-script.rb $OO_LOCAL{ES_HOST} `date +%Y-%m-%d --date='-1 day'` `date +%Y-%m-%d --date='-1 day'` 2>&1 >>/opt/oneops/log/cost_batch_status.log && break
       n=$[$n+1]
-      mail -s "$en :: daily cost indexer failed for `date +%Y-%m-%d --date='-1 day'` at `date`. Retry attempt $n " rajanand@walmartlabs.com
       sleep 15
    done
