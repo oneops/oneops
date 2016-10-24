339798086dea699e53232b1d7acdfb5a5b812eea
Kevin Traver <kevintraver@gmail.com>
2016-10-24 14:58:51 -0700

    Transition pull design - multiple environments

--- app/controllers/transition_controller.rb
+++ app/controllers/transition_controller.rb
@@ -1,6 +1,7 @@
 class TransitionController < ApplicationController
+  before_filter :find_assembly
+
   def show
-    @assembly     = locate_assembly(params[:assembly_id])
     @environments = Cms::Relation.all(:params => {:ciId              => @assembly.ciId,
                                                   :direction         => 'from',
                                                   :relationShortName => 'RealizedIn',
@@ -17,7 +18,6 @@ class TransitionController < ApplicationController
                                                   :includeToCi   => true,
                                                   :recursive     => true})
 
-    @catalog = Cms::Release.latest(:nsPath => assembly_ns_path(@assembly), :releaseState => 'closed')
 
     # We try to get deployment and bom release data from ES for performance reasons. But if it fails (ES is unavailable)
     # then we fall back to old inefficient way from CMS.
@@ -62,5 +62,57 @@ class TransitionController < ApplicationController
 
     @profiles = Cms::Ci.all(:params => {:nsPath      => organization_ns_path,
                                         :ciClassName => 'account.Environment'}).sort_by(&:ciName)
+
+    respond_to do |format|
+      format.html
+      format.js { render :action => :show }
+    end
+
   end
+
+  def pull
+    environment_ids = params["ciIds"]
+    status = environment_ids.collect do |environment_id|
+      @environment = locate_environment(environment_id, @assembly) if environment_id
+      manifest_ns_path = environment_manifest_ns_path(@environment)
+
+      @release  = Cms::Release.latest(:nsPath => manifest_ns_path)
+      @manifest = @release && @release.releaseState == 'canceled' ? Cms::Release.latest(:nsPath => manifest_ns_path, :releaseState => 'closed') : @release
+
+      design_pull_in_progress = @environment.ciState == 'manifest_locked'
+      open_manifest_release = @manifest && @manifest.releaseState == 'open'
+      design_latest = @manifest && @manifest.parentReleaseId == @catalog.releaseId
+
+      if open_manifest_release
+        {:environment => @environment.ciName, :status => 'Current release not committed', :success => false}
+      elsif design_pull_in_progress
+        {:environment => @environment.ciName, :status => 'Design pull in progress', :success => false}
+      else
+        release_id, error = Transistor.pull_design(@environment.ciId)
+        if error
+          {:environment => @environment.ciName, :status => error, :success => false}
+        else
+          {:environment => @environment.ciName, :status => 'pull initiated', :success => true}
+        end
+      end
+    end
+
+    if status.all? {|s| s[:success]}
+      message = "Pull initiated"
+      flash[:notice] = message
+    else
+      message = status.select{|s| !s[:success]}.map{|s| "#{s[:environment]}: #{s[:status]}"}.join("<br/>").html_safe
+      flash[:error] = message
+    end
+
+    show
+  end
+
+  private
+
+  def find_assembly
+    @assembly = locate_assembly(params[:assembly_id])
+    @catalog = Cms::Release.latest(:nsPath => assembly_ns_path(@assembly), :releaseState => 'closed')
+  end
+
 end
