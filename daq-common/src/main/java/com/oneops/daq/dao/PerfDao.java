21f66adae9e822c963df9220eb0a66ca8c7ba361
Suresh G <sgopal1@walmartlabs.com>
2016-10-25 18:03:47 -0700

    ldb and notification_ks cleanup from oneops core.

--- daq-common/src/main/java/com/oneops/daq/dao/PerfDao.java
+++ daq-common/src/main/java/com/oneops/daq/dao/PerfDao.java
@@ -1,20 +1,21 @@
 /*******************************************************************************
- *  
+ *
  *   Copyright 2015 Walmart, Inc.
- *  
+ *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
  *   You may obtain a copy of the License at
- *  
+ *
  *       http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *   Unless required by applicable law or agreed to in writing, software
  *   distributed under the License is distributed on an "AS IS" BASIS,
  *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
- *  
+ *
  *******************************************************************************/
+
 package com.oneops.daq.dao;
 
 import com.oneops.daq.Util;
@@ -44,10 +45,7 @@ import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
- *         PerfDao - encapsulates cassandra data access for performance data
- *         clients:
- *         write: PerfSink uses process() and constructor or property setters for cassandra connection
- *         read: web.PerfAndLogController uses getPerfData() getLogData() ; getPerfAvailable() getLogAvailable()
+ * PerfDao - encapsulates cassandra data access for performance data.
  */
 @Component
 public class PerfDao implements CassandraConstants {
