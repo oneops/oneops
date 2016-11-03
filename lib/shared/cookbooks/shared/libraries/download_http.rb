ae9f6794df5e2f78184973a0f98848890b889284
Phat Huynh <sleepyorion@gmail.com>
2016-11-03 15:33:14 -0700

    refactor download_http to make it compatible with latest artifact change

--- lib/shared/cookbooks/shared/libraries/download_http.rb
+++ lib/shared/cookbooks/shared/libraries/download_http.rb
@@ -1,4 +1,5 @@
 require 'chef/provider/remote_file'
+require 'fileutils'
 
 class Chef
   class Provider
@@ -17,33 +18,25 @@ class Chef
           puts "existing file matches checksum."
           Chef::Log.info("#{@new_resource} checksum matches target checksum (#{@new_resource.checksum}) - not updating")
         else
-
           mirrors = @new_resource.source.split(",")
           mirrors.each do |mirror|
             begin
               url = URI.parse(mirror)
               url.user, url.password = @new_resource.basic_auth_user, @new_resource.basic_auth_password if @new_resource.basic_auth_user
-              Chef::REST.new(mirror, nil, nil).streaming_request(url, @new_resource.headers) do |raw_file|
+              rest = Chef::REST.new(url.to_s,nil, nil, http_client_opts(url.to_s))
+              raw_file = rest.streaming_request(url,@new_resource.headers,@new_resource.path)
+              raw_checksum = Chef::Digester.checksum_for_file(raw_file.path) if !desired_checksum.empty?
 
-                raw_checksum = Chef::Digester.checksum_for_file(raw_file.path) if !desired_checksum.empty?
-                #lsl = `ls -l #{raw_file.path}`
-                #Chef::Log.info("Downloaded file: #{lsl}")
-                Chef::Log.info("Downloaded file checksum: #{raw_checksum}")
- 
-                if desired_checksum.empty? || desired_checksum == raw_checksum
-                  Chef::Log.info("Moving to: #{@new_resource.path} ")
-                  # not working from java attachment, so using `mv`
-                  #FileUtils.cp raw_file.path, @new_resource.path
-                  `mv #{raw_file.path} #{@new_resource.path}`
-                  lsl = `ls -l #{@new_resource.path}`
-                  Chef::Log.info("Downloaded file: #{lsl}")
-                else
-                  Chef::Log.error("Checksum of downloaded file: #{raw_checksum} expecting: #{@new_resource.checksum}")
-                  exit 1
-                end
-                                
+              Chef::Log.info("Downloaded file checksum: #{raw_checksum}")
+
+              if desired_checksum.empty? || desired_checksum == raw_checksum
+                Chef::Log.info("Moving to: #{@new_resource.path} ")
+                FileUtils.mv(raw_file.path, @new_resource.path)
+                Chef::Log.info("Downloaded file: #{@new_resource.path}")
+              else
+                Chef::Log.error("Checksum of downloaded file: #{raw_checksum} expecting: #{@new_resource.checksum}")
+                exit 1
               end
-
               @new_resource.updated_by_last_action(true)
               # successful download or checksum match
               break
@@ -53,15 +46,11 @@ class Chef
                 exit 1
               end
             end
-
           end
-
         end
         enforce_ownership_and_permissions
-
         @new_resource.updated_by_last_action?
       end
-
     end
   end
 end
