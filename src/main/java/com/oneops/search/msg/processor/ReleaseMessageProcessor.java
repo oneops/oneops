e6787a80831e43dd11b939241c5ed47351f77c99
Oleg Kornev <kornev@gmail.com>
2016-11-10 11:05:51 -0500

    fix for

--- src/main/java/com/oneops/search/msg/processor/ReleaseMessageProcessor.java
+++ src/main/java/com/oneops/search/msg/processor/ReleaseMessageProcessor.java
@@ -1,6 +1,11 @@
 package com.oneops.search.msg.processor;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.oneops.cms.dj.domain.CmsRelease;
+import com.oneops.cms.util.CmsConstants;
 import com.oneops.search.msg.index.Indexer;
 import org.apache.http.client.fluent.Request;
 import org.apache.log4j.Logger;
@@ -42,11 +47,20 @@ public class ReleaseMessageProcessor implements MessageProcessor {
         indexer.indexEvent(RELEASE, releaseMsg);
         indexer.index(String.valueOf(release.getReleaseId()), RELEASE, releaseMsg);
         try {
-            String url = snapshotURL + release.getNsPath();
-            logger.info("Retrieving snapshot for:" + url);
-            message = Request.Get(url).execute().returnContent().asString();
-            indexer.index(String.valueOf(release.getReleaseId()), "snapshot", message);
-            logger.info("Snapshot indexed");
+            if ("closed".equalsIgnoreCase(release.getReleaseState())) {
+                String url = snapshotURL + release.getNsPath();
+                logger.info("Retrieving snapshot for:" + url + " expected release:" + release.getReleaseId());
+                message = Request.Get(url).addHeader("Content-Type", "application/json").execute().returnContent().asString();
+                long releaseId = new JsonParser().parse(message).getAsJsonObject().get("release").getAsLong();
+                if (releaseId > release.getReleaseId()) {
+                    logger.warn("Snapshot is dirty, so discarding. Was expecting release:" + release.getReleaseId() + " snapshot is for:" + releaseId);
+                } else {
+                    indexer.index(String.valueOf(release.getReleaseId()), "snapshot", message);
+                    logger.info("Snapshot indexed for release ID: " + releaseId);
+                }
+            } else {
+                logger.info("Release is not closed. Won't do snapshot");
+            }
         } catch (Exception e) {
             logger.error("Error while retrieving snapshot" + e.getMessage(), e);
         }
