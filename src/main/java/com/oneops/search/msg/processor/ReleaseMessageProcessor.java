4380018299ebfcf59171b72f82be2cb1ea79190c
Oleg Kornev <kornev@gmail.com>
2016-11-11 18:49:47 -0500

    fix for

--- src/main/java/com/oneops/search/msg/processor/ReleaseMessageProcessor.java
+++ src/main/java/com/oneops/search/msg/processor/ReleaseMessageProcessor.java
@@ -1,11 +1,7 @@
 package com.oneops.search.msg.processor;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.oneops.cms.dj.domain.CmsRelease;
-import com.oneops.cms.util.CmsConstants;
 import com.oneops.search.msg.index.Indexer;
 import org.apache.http.client.fluent.Request;
 import org.apache.log4j.Logger;
@@ -48,24 +44,28 @@ public class ReleaseMessageProcessor implements MessageProcessor {
         String releaseMsg = GSON_ES.toJson(release);
         indexer.indexEvent(RELEASE, releaseMsg);
         indexer.index(String.valueOf(release.getReleaseId()), RELEASE, releaseMsg);
+        indexSnapshot(release);
+    }
+
+    public void indexSnapshot(CmsRelease release) {
         try {
-            if ("closed".equalsIgnoreCase(release.getReleaseState())) {
-                if (release.getNsPath().endsWith("bom")) return; // ignore bom release
-                
-                String url = (release.getNsPath().endsWith("manifest")?manifestSnapshotURL:designSnapshotURL) + release.getNsPath();  // set snapshot URL based on release type
-                
-                logger.info("Retrieving snapshot for:" + url + " expected release:" + release.getReleaseId());
-                message = Request.Get(url).addHeader("Content-Type", "application/json").execute().returnContent().asString();
-                long releaseId = new JsonParser().parse(message).getAsJsonObject().get("release").getAsLong();
-                if (releaseId > release.getReleaseId()) {
-                    logger.warn("Snapshot is dirty, so discarding. Was expecting release:" + release.getReleaseId() + " snapshot has rfcs from release:" + releaseId);
-                } else {
-                    indexer.index(String.valueOf(release.getReleaseId()), "snapshot", message);
-                    logger.info("Snapshot indexed for release ID: " + release.getReleaseId());
-                }
-            } else {
+            if (!"closed".equalsIgnoreCase(release.getReleaseState())) {
                 logger.info("Release is not closed. Won't do snapshot");
+                return;
             }
+            if (release.getNsPath().endsWith("bom")) return; // ignore bom release
+
+            String url = (release.getNsPath().endsWith("manifest") ? manifestSnapshotURL : designSnapshotURL) + release.getNsPath();  // set snapshot URL based on release type
+            logger.info("Retrieving snapshot for:" + url + " expected release:" + release.getReleaseId());
+            String message = Request.Get(url).addHeader("Content-Type", "application/json").execute().returnContent().asString();
+            long releaseId = new JsonParser().parse(message).getAsJsonObject().get("release").getAsLong();
+            if (releaseId > release.getReleaseId()) {
+                logger.warn("Snapshot is dirty, so discarding. Was expecting release:" + release.getReleaseId() + " snapshot has rfcs from release:" + releaseId);
+            } else {
+                indexer.index(String.valueOf(release.getReleaseId()), "snapshot", message);
+                logger.info("Snapshot indexed for release ID: " + release.getReleaseId());
+            }
+
         } catch (Exception e) {
             logger.error("Error while retrieving snapshot" + e.getMessage(), e);
         }
