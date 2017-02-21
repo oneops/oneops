734a5e791054473783240f32f007fd94137d6429
Lev Khusid <lkhusid@gmail.com>
2017-02-21 11:29:56 -0500

    Adjustments to release restore to show potential warnings on successful restore.

--- app/controllers/base/releases_controller.rb
+++ app/controllers/base/releases_controller.rb
@@ -1,11 +1,12 @@
 class Base::ReleasesController < ApplicationController
   def restore
-    label      = @environment ? 'environment' : 'design'
     ns_path    = @environment ? environment_manifest_ns_path(@environment) : assembly_ns_path(@assembly)
     @release   = Cms::Release.latest(:nsPath => ns_path)
     release_id = params[:id].to_i
     snapshot   = nil
     message    = nil
+    ok         = false
+
     begin
       snapshot = Search::Snapshot.find_by_ns_and_release_id(ns_path, release_id)
     rescue Exception => e
@@ -17,30 +18,29 @@ class Base::ReleasesController < ApplicationController
         message = 'There is already an open release.  Please discard current open release first before requesting restoration of a prior release.'
       else
         ok, message = Transistor.restore_release(snapshot, release_id)
-        @release    = Cms::Release.latest(:nsPath => ns_path) if ok
+        if ok
+          @release = Cms::Release.latest(:nsPath => ns_path)
+          @warnings = message
+        end
       end
     elsif !message
-      message = "Unable to restore release: could not find any prior #{label} snapshots. Release restore feature is only available for releases committed after Jan, 11, 2017."
+      message = "Unable to restore release: could not find any prior #{@environment ? 'environment' : 'design'} snapshots. Release restore feature is only available for releases committed after Jan, 11, 2017."
     end
-
     respond_to do |format|
       format.js do
-        if message
+        if ok
+          render 'base/releases/restore'
+        else
           flash[:error] = message
           render :js => ''
-        elsif @release.releaseState != 'open'
-            flash[:alert] = "Restore release request resulted in no changes to the curret #{label}."
-            render :js => 'hide_modal();'
-        else
-          render :js => "hide_modal(); window.location = '#{path_to_release(@release)}'"
         end
       end
 
       format.json do
-        if message
-          render :json => {:errors => [message]}, :status => :unprocessable_entity
+        if ok
+          render :json => {:release => @release, :warnings => [message]}
         else
-          render :json => @release
+          render :json => {:errors => [message]}, :status => :unprocessable_entity
         end
       end
     end
