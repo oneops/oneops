413c9a49a4a743e4bd7e84c0f956f5304c2b0125
Gaurav Lall <glall@walmartlabs.com>
2017-03-16 01:32:55 -0700

    Added support for getting the version info to search tags to get metrics/perf  across releases.  Removed Locking in favour of concurrent HashMap

--- src/main/java/com/oneops/controller/jms/InductorPublisher.java
+++ src/main/java/com/oneops/controller/jms/InductorPublisher.java
@@ -18,8 +18,8 @@ package com.oneops.controller.jms;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.jms.Connection;
 import javax.jms.DeliveryMode;
@@ -29,10 +29,12 @@ import javax.jms.MessageProducer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 
+import com.oneops.util.Version;
 import org.activiti.engine.delegate.DelegateExecution;
 import org.apache.activemq.ActiveMQConnection;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.util.IndentPrinter;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 
 import com.google.gson.Gson;
@@ -40,15 +42,17 @@ import com.oneops.cms.domain.CmsWorkOrderSimpleBase;
 import com.oneops.cms.simple.domain.CmsActionOrderSimple;
 import com.oneops.cms.simple.domain.CmsWorkOrderSimple;
 import com.oneops.cms.util.CmsConstants;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * The Class InductorPublisher.
  */
 public class InductorPublisher {
+	private static final String DEFAULT_VERSION = "1.0.0";
 	private static Logger logger = Logger.getLogger(InductorPublisher.class);
 	
 	//private long timeToLive;
-    private Map<String, MessageProducer> bindingQueusMap = new HashMap<String, MessageProducer>();
+    private Map<String, MessageProducer> bindingQueusMap = new ConcurrentHashMap<>();
     private static final String QUEUE_SUFFIX = ".ind-wo";
     private static final String SHARED_QUEUE = "shared" + QUEUE_SUFFIX;
     private static final String USE_SHARED_FLAG = "com.oneops.controller.use-shared-queue";
@@ -78,7 +82,7 @@ public class InductorPublisher {
             connection = connFactory.createConnection();
             session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
             connection.start();
-            logger.info(">>>>>>>>>>>>>>InductorPublisher initalized...");
+            logger.info(">>>>>>>>>>>>>>InductorPublisher initialized...");
     }
 
     
@@ -97,7 +101,9 @@ public class InductorPublisher {
     	CmsWorkOrderSimpleBase wo = (CmsWorkOrderSimpleBase)exec.getVariable("wo");
     	publishMessage(processId, execId, wo, waitTaskName, woType);
     }
-    
+
+    @Autowired
+	Version version;
     /**
      * Publish message.
      *
@@ -106,10 +112,14 @@ public class InductorPublisher {
      * @throws JMSException the jMS exception
      */
     public void publishMessage(String processId, String execId, CmsWorkOrderSimpleBase wo, String waitTaskName, String woType) throws JMSException {
-		SimpleDateFormat format=  new SimpleDateFormat(CmsConstants.SEARCH_TS_PATTERN);
-    	wo.getSearchTags().put(CmsConstants.REQUEST_ENQUE_TS, format.format(new Date()));
-    	
-    	synchronized(lock) {
+		    SimpleDateFormat format=  new SimpleDateFormat(CmsConstants.SEARCH_TS_PATTERN);
+    	    wo.getSearchTags().put(CmsConstants.REQUEST_ENQUE_TS, format.format(new Date()));
+		    //guarantee non empty-value for searchMap
+    	    if(version!=null && StringUtils.isNotBlank(version.getGitVersion())){
+		    	wo.getSearchTags().put("ctrlrVersion", version.getGitVersion());
+			}else{
+				wo.getSearchTags().put("ctrlrVersion", DEFAULT_VERSION);
+			}
 	    	TextMessage message = session.createTextMessage(gson.toJson(wo));
 	    	String corelationId = processId + "!" + execId + "!" + waitTaskName;
 	    	message.setJMSCorrelationID(corelationId);
@@ -121,10 +131,7 @@ public class InductorPublisher {
 	    	} else {
 	    		queueName = (wo.getCloud().getCiAttributes().get("location").replaceAll("/", ".") + QUEUE_SUFFIX).substring(1);
 	    	}
-	    	if (!bindingQueusMap.containsKey(queueName)) {
-	    		bindingQueusMap.put(queueName, newMessageProducer(queueName));
-	    	}
-	    	bindingQueusMap.get(queueName).send(message);
+	    	bindingQueusMap.putIfAbsent(queueName,newMessageProducer(queueName)).send(message);
 	    	if (wo instanceof CmsWorkOrderSimple) {
 	    		logger.info("Posted wo for the inductor dpmtId=" + ((CmsWorkOrderSimple)wo).getDeploymentId() 
 	    				  + "; rfcId=" + ((CmsWorkOrderSimple)wo).getRfcId() 
@@ -137,8 +144,6 @@ public class InductorPublisher {
 	    	woPublisher.publishMessage(wo,woType,woCorelationId);
 	    	
 	    	logger.debug("Published: "+message.getText());
-	    	lock.notifyAll();
-   		}
     }
     
 	private MessageProducer newMessageProducer(String queueName) throws JMSException {
@@ -147,6 +152,7 @@ public class InductorPublisher {
         // Create the producer.
         MessageProducer producer = session.createProducer(destination);
         producer.setDeliveryMode(DeliveryMode.PERSISTENT);
+    	logger.info("Created message producer for queue "+queueName);
         return producer;
     }
 	
