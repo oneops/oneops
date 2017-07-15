9ecb05c3d918feb676cc992b654d846c9bf779c1
rrupaku <rrupaku@wal-mart.com>
2017-07-15 21:57:35 +0530

    adding windows support for check_process_count.sh counter

--- oneops-admin/lib/shared/cookbooks/monitor/files/default/check_process_count.sh
+++ oneops-admin/lib/shared/cookbooks/monitor/files/default/check_process_count.sh
@@ -2,16 +2,11 @@
 PROCESS=$1
 
 if [[ `uname` == *CYGWIN* ]]; then
-  out=`powershell "(get-service $PROCESS).Status"`
-  if [[ "$out" == *Stopped* ]]; then
-	 echo "$PROCESS Count |count=0"
-  else
-     echo "$PROCESS Count |count=100"
-  fi
-  exit
+  out=`powershell "ps $PROCESS -ErrorAction SilentlyContinue | wc -l"`
+else
+  out=`ps auxwww|egrep $PROCESS| grep -v grep| grep -v check_process_count | wc -l`
 fi
 
-out=`ps auxwww|egrep $PROCESS| grep -v grep| grep -v check_process_count | wc -l`
 echo $out
 echo "$PROCESS Count |count=$out"
 exit $?
