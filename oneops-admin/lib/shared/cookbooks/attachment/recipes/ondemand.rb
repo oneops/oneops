4aa443eec34e324f044070b2799fe37890aa5e3b
Cody Lee White <cody.white0@walmart.com>
2017-07-27 09:41:33 -0500

    added kitchen stuff for attachment

--- oneops-admin/lib/shared/cookbooks/attachment/recipes/ondemand.rb
+++ oneops-admin/lib/shared/cookbooks/attachment/recipes/ondemand.rb
@@ -43,7 +43,7 @@ if node.workorder.payLoad.has_key?('EscortedBy')
 
       _headers = _headers.empty? ? Hash.new : JSON.parse(_headers)
       _checksum = a[:ciAttributes][:checksum] or nil
-
+      
       shared_download_http "#{_source}" do
         path _path
         checksum _checksum
@@ -66,7 +66,6 @@ if node.workorder.payLoad.has_key?('EscortedBy')
         action :create
         only_if do _source =~ /s3:\/\// end
       end
-
     end
 
     if a[:ciAttributes].has_key?("exec_cmd")
