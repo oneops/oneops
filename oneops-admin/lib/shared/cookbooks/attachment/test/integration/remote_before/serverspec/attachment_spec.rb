b9fecbf2ebb43dbad125dece7a60aff692b9a27b
Cody Lee White <cody.white0@walmart.com>
2017-08-23 10:29:29 -0500

    added kitchen tests for attachment

---
+++ oneops-admin/lib/shared/cookbooks/attachment/test/integration/remote_before/serverspec/attachment_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe file('/tmp/download_file') do
+  puts "------remote_before-------"
+  it { should exist }
+  it { should be_executable }
+end
+describe command('/tmp/download_file') do
+  its(:exit_status) { should eq 0 }
+end
