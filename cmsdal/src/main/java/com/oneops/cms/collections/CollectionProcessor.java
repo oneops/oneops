864d4b2f2f176cb2584e9c13f0fab56f56b04cba
Gaurav Lall <glall@walmartlabs.com>
2017-09-14 20:44:20 -0700

    Fix formatting

--- cmsdal/src/main/java/com/oneops/cms/collections/CollectionProcessor.java
+++ cmsdal/src/main/java/com/oneops/cms/collections/CollectionProcessor.java
@@ -36,153 +36,131 @@ public class CollectionProcessor {
 
   private static final String RELATION_ATTRIBUTE_SUFFIX = ".";
   private CmsCmRfcMrgProcessor cmrfcProcessor;
-  private CmsCmProcessor cmProcessor;
+	private CmsCmProcessor cmProcessor;
 
-  /**
-   * Sets the cmrfc processor.
-   *
-   * @param cmrfcProcessor the new cmrfc processor
-   */
-  public void setCmrfcProcessor(CmsCmRfcMrgProcessor cmrfcProcessor) {
-    this.cmrfcProcessor = cmrfcProcessor;
-  }
+	/**
+	 * Sets the cmrfc processor.
+	 *
+	 * @param cmrfcProcessor the new cmrfc processor
+	 */
+	public void setCmrfcProcessor(CmsCmRfcMrgProcessor cmrfcProcessor) {
+		this.cmrfcProcessor = cmrfcProcessor;
+	}
 
-  /**
-   * Sets the cm processor.
-   *
-   * @param cmProcessor the new cm processor
-   */
-  public void setCmProcessor(CmsCmProcessor cmProcessor) {
-    this.cmProcessor = cmProcessor;
-  }
+	/**
+	 * Sets the cm processor.
+	 *
+	 * @param cmProcessor the new cm processor
+	 */
+	public void setCmProcessor(CmsCmProcessor cmProcessor) {
+		this.cmProcessor = cmProcessor;
+	}
 
-  /**
-   * Gets the flat collection.
-   *
-   * @param anchorCiId the anchor ci id
-   * @param relationDef the relation def
-   * @return the flat collection
-   */
-  public List<CmsCI> getFlatCollection(long anchorCiId, CollectionLinkDefinition relationDef) {
+	/**
+	 * Gets the flat collection.
+	 *
+	 * @param anchorCiId the anchor ci id
+	 * @param relationDef the relation def
+	 * @return the flat collection
+	 */
+	public List<CmsCI> getFlatCollection(long anchorCiId, CollectionLinkDefinition relationDef) {
 
-    List<CmsCI> result = new ArrayList<CmsCI>();
+		List<CmsCI> result = new ArrayList<CmsCI>();
 
-    if ("from".equalsIgnoreCase(relationDef.getDirection())) {
-      List<CmsCIRelation> relations = null;
-      if (relationDef.getRelationAttrs() != null && relationDef.getRelationAttrs().size() > 0) {
-        relations = cmProcessor
-            .getFromCIRelationsByAttrsNaked(anchorCiId, relationDef.getRelationName(),
-                relationDef.getRelationShortName(), relationDef.getTargetClassName(),
-                relationDef.getRelationAttrs());
-      } else {
-        relations = cmProcessor
-            .getFromCIRelationsNakedNoAttrs(anchorCiId, relationDef.getRelationName(),
-                relationDef.getRelationShortName(), relationDef.getTargetClassName());
-      }
-      for (CmsCIRelation rel : relations) {
-        if (relationDef.getReturnObject()) {
-          final CmsCI aCi = cmProcessor.getCiById(rel.getToCiId());
-          result.add(addRelationAttributes(relationDef, rel, aCi));
-        }
-        if (relationDef.getRelations() != null) {
-          for (CollectionLinkDefinition relDef : relationDef.getRelations()) {
-            result.addAll(getFlatCollection(rel.getToCiId(), relDef));
-          }
-        }
-      }
-    } else {
+		if ("from".equalsIgnoreCase(relationDef.getDirection())) {
+			List<CmsCIRelation> relations = null;
+			if (relationDef.getRelationAttrs() != null && relationDef.getRelationAttrs().size()>0 ) {
+				relations = cmProcessor.getFromCIRelationsByAttrsNaked(anchorCiId, relationDef.getRelationName(), relationDef.getRelationShortName(), relationDef.getTargetClassName(), relationDef.getRelationAttrs());
+			} else {
+				relations = cmProcessor.getFromCIRelationsNakedNoAttrs(anchorCiId, relationDef.getRelationName(), relationDef.getRelationShortName(), relationDef.getTargetClassName());
+			}
+			for (CmsCIRelation rel : relations) {
+				if (relationDef.getReturnObject()) {
+					final CmsCI aCi = cmProcessor.getCiById(rel.getToCiId());
+					result.add(addRelationAttributes(relationDef,rel,aCi));
+				}
+				if (relationDef.getRelations() != null) {
+					for (CollectionLinkDefinition relDef : relationDef.getRelations()) {
+						result.addAll(getFlatCollection(rel.getToCiId(), relDef));
+					}
+				}
+			}
+		} else {
 
-      List<CmsCIRelation> relations = null;
-      if (relationDef.getRelationAttrs() != null && relationDef.getRelationAttrs().size() > 0) {
-        relations = cmProcessor
-            .getToCIRelationsByAttrsNaked(anchorCiId, relationDef.getRelationName(),
-                relationDef.getRelationShortName(), relationDef.getTargetClassName(),
-                relationDef.getRelationAttrs());
-      } else {
-        relations = cmProcessor
-            .getToCIRelationsNakedNoAttrs(anchorCiId, relationDef.getRelationName(),
-                relationDef.getRelationShortName(), relationDef.getTargetClassName());
-      }
-      for (CmsCIRelation rel : relations) {
-        if (relationDef.getReturnObject()) {
-          CmsCI aCi = cmProcessor.getCiById(rel.getFromCiId());
-          result.add(addRelationAttributes(relationDef, rel, aCi));
-        }
-        if (relationDef.getRelations() != null) {
-          for (CollectionLinkDefinition relDef : relationDef.getRelations()) {
-            result.addAll(getFlatCollection(rel.getFromCiId(), relDef));
-          }
-        }
-      }
-    }
+			List<CmsCIRelation> relations = null;
+			if (relationDef.getRelationAttrs() != null && relationDef.getRelationAttrs().size()>0 ) {
+				relations = cmProcessor.getToCIRelationsByAttrsNaked(anchorCiId, relationDef.getRelationName(), relationDef.getRelationShortName(), relationDef.getTargetClassName(),relationDef.getRelationAttrs());
+			} else {
+				relations = cmProcessor.getToCIRelationsNakedNoAttrs(anchorCiId, relationDef.getRelationName(), relationDef.getRelationShortName(), relationDef.getTargetClassName());
+			}
+			for (CmsCIRelation rel : relations) {
+				if (relationDef.getReturnObject()) {
+					CmsCI aCi = cmProcessor.getCiById(rel.getFromCiId());
+					result.add(addRelationAttributes(relationDef,rel,aCi));
+				}
+				if (relationDef.getRelations() != null) {
+					for (CollectionLinkDefinition relDef : relationDef.getRelations()) {
+						result.addAll(getFlatCollection(rel.getFromCiId(), relDef));
+					}
+				}
+			}
+		}
 
-    return result;
-  }
+		return result;
+	}
 
-  /**
-   * Gets the flat collection rfc.
-   *
-   * @param anchorCiId the anchor ci id
-   * @param relationDef the relation def
-   * @return the flat collection rfc
-   */
-  public List<CmsRfcCI> getFlatCollectionRfc(long anchorCiId,
-      CollectionLinkDefinition relationDef) {
+	/**
+	 * Gets the flat collection rfc.
+	 * @param anchorCiId the anchor ci id
+	 * @param relationDef the relation def
+	 * @return the flat collection rfc
+	 */
+	public List<CmsRfcCI> getFlatCollectionRfc(long anchorCiId, CollectionLinkDefinition relationDef) {
 
-    List<CmsRfcCI> result = new ArrayList<>();
+		List<CmsRfcCI> result = new ArrayList<>();
 
-    if ("from".equalsIgnoreCase(relationDef.getDirection())) {
-      List<CmsRfcRelation> relations = null;
-      if (relationDef.getRelationAttrs() != null && relationDef.getRelationAttrs().size() > 0) {
-        relations = cmrfcProcessor
-            .getFromCIRelationsByAttrs(anchorCiId, relationDef.getRelationName(),
-                relationDef.getRelationShortName(), relationDef.getTargetClassName(), "df",
-                relationDef.getRelationAttrs());
-      } else {
-        relations = cmrfcProcessor
-            .getFromCIRelationsNakedNoAttrs(anchorCiId, relationDef.getRelationName(),
-                relationDef.getRelationShortName(), relationDef.getTargetClassName());
-      }
-      for (CmsRfcRelation rel : relations) {
-        if (relationDef.getReturnObject()) {
-          CmsRfcCI aRfc = cmrfcProcessor.getCiById(rel.getToCiId(), "df");
-          result.add(addRelationRfcAttributes(relationDef, rel, aRfc));
-        }
-        //
-        if (relationDef.getRelations() != null) {
-          for (CollectionLinkDefinition relDef : relationDef.getRelations()) {
-            result.addAll(getFlatCollectionRfc(rel.getToCiId(), relDef));
-          }
-        }
-      }
-    } else {
-      List<CmsRfcRelation> relations = null;
-      if (relationDef.getRelationAttrs() != null && relationDef.getRelationAttrs().size() > 0) {
-        relations = cmrfcProcessor
-            .getToCIRelationsByAttrs(anchorCiId, relationDef.getRelationName(),
-                relationDef.getRelationShortName(), relationDef.getTargetClassName(), "df",
-                relationDef.getRelationAttrs());
-      } else {
-        relations = cmrfcProcessor
-            .getToCIRelationsNakedNoAttrs(anchorCiId, relationDef.getRelationName(),
-                relationDef.getRelationShortName(), relationDef.getTargetClassName());
-      }
-      for (CmsRfcRelation rel : relations) {
-        if (relationDef.getReturnObject()) {
-          CmsRfcCI aRfc = cmrfcProcessor.getCiById(rel.getFromCiId(), "df");
-          result.add(addRelationRfcAttributes(relationDef, rel, aRfc));
+		if ("from".equalsIgnoreCase(relationDef.getDirection())) {
+			List<CmsRfcRelation> relations = null;
+			if (relationDef.getRelationAttrs() != null && relationDef.getRelationAttrs().size()>0 ) {
+				relations = cmrfcProcessor.getFromCIRelationsByAttrs(anchorCiId, relationDef.getRelationName(), relationDef.getRelationShortName(), relationDef.getTargetClassName(), "df", relationDef.getRelationAttrs());
+			} else {
+				relations = cmrfcProcessor.getFromCIRelationsNakedNoAttrs(anchorCiId, relationDef.getRelationName(), relationDef.getRelationShortName(), relationDef.getTargetClassName());
+			}
+			for (CmsRfcRelation rel : relations) {
+				if (relationDef.getReturnObject()) {
+					CmsRfcCI aRfc= cmrfcProcessor.getCiById(rel.getToCiId(),"df");
+					result.add(addRelationRfcAttributes(relationDef,rel,aRfc ));
+				}
+				//
+				if (relationDef.getRelations() != null) {
+					for (CollectionLinkDefinition relDef : relationDef.getRelations()) {
+						result.addAll(getFlatCollectionRfc(rel.getToCiId(), relDef));
+					}
+				}
+			}
+		} else {
+			List<CmsRfcRelation> relations = null;
+			if (relationDef.getRelationAttrs() != null && relationDef.getRelationAttrs().size()>0 ) {
+				relations = cmrfcProcessor.getToCIRelationsByAttrs(anchorCiId, relationDef.getRelationName(), relationDef.getRelationShortName(), relationDef.getTargetClassName(), "df", relationDef.getRelationAttrs());
+			} else {
+				relations = cmrfcProcessor.getToCIRelationsNakedNoAttrs(anchorCiId, relationDef.getRelationName(), relationDef.getRelationShortName(), relationDef.getTargetClassName());
+			}
+			for (CmsRfcRelation rel : relations) {
+				if (relationDef.getReturnObject()) {
+					CmsRfcCI aRfc= cmrfcProcessor.getCiById(rel.getFromCiId(), "df");
+					result.add(addRelationRfcAttributes(relationDef,rel,aRfc ));
 
-        }
-        if (relationDef.getRelations() != null) {
-          for (CollectionLinkDefinition relDef : relationDef.getRelations()) {
-            result.addAll(getFlatCollectionRfc(rel.getFromCiId(), relDef));
-          }
-        }
-      }
-    }
+				}
+				if (relationDef.getRelations() != null) {
+					for (CollectionLinkDefinition relDef : relationDef.getRelations()) {
+						result.addAll(getFlatCollectionRfc(rel.getFromCiId(), relDef));
+					}
+				}
+			}
+		}
 
-    return result;
-  }
+		return result;
+	}
 
 
   private CmsRfcCI addRelationRfcAttributes(CollectionLinkDefinition relationDef,
@@ -202,74 +180,75 @@ public class CollectionProcessor {
   }
 
 
-  private CmsCI addRelationAttributes(CollectionLinkDefinition relationDef, CmsCIRelation rel,
-      CmsCI aCi) {
-    if (relationDef.getReturnRelationAttributes()) {
-      rel.getAttributes().forEach((k, v) -> {
-        CmsCIAttribute attribute = new CmsCIAttribute();
-        attribute.setAttributeName(rel.getRelationName() + RELATION_ATTRIBUTE_SUFFIX + k);
-        attribute.setDfValue(v.getDfValue());
-        attribute.setDjValue(v.getDjValue());
-        aCi.getAttributes().put(attribute.getAttributeName(), attribute);
-      });
-    }
-    return aCi;
-  }
+
+	private CmsCI addRelationAttributes(CollectionLinkDefinition relationDef, CmsCIRelation rel,
+			CmsCI aCi) {
+		if(relationDef.getReturnRelationAttributes()){
+			rel.getAttributes().forEach((k,v)->{
+				CmsCIAttribute attribute = new CmsCIAttribute();
+				attribute.setAttributeName(rel.getRelationName()+ RELATION_ATTRIBUTE_SUFFIX +k);
+				attribute.setDfValue(v.getDfValue());
+				attribute.setDjValue(v.getDjValue());
+				aCi.getAttributes().put(attribute.getAttributeName(),attribute);
+			});
+		}
+		return aCi;
+	}
 
 
   /**
-   * Upsert collection rfc.
-   *
-   * @param rfcNode the rfc node
-   * @param userId the user id
-   */
-  public void upsertCollectionRfc(CollectionRfcNode rfcNode, String userId) {
-    CmsRfcCI rfc = cmrfcProcessor.upsertCiRfc(rfcNode, userId);
+     * Upsert collection rfc.
+     *
+     * @param rfcNode the rfc node
+     * @param userId the user id
+     */
+    public void upsertCollectionRfc(CollectionRfcNode rfcNode, String userId) {
+    	CmsRfcCI rfc = cmrfcProcessor.upsertCiRfc(rfcNode, userId);
 
-    for (CollectionRfcLink link : rfcNode.getRelations()) {
-      link.setFromCiId(rfc.getCiId());
-      link.setFromRfcId(rfc.getRfcId());
-      cmrfcProcessor.upsertRelationRfc(link, userId);
-      upsertCollectionRfc(link.getLinkedNode(), userId);
+        for(CollectionRfcLink link: rfcNode.getRelations()) {
+        	link.setFromCiId(rfc.getCiId());
+        	link.setFromRfcId(rfc.getRfcId());
+            cmrfcProcessor.upsertRelationRfc(link, userId);
+            upsertCollectionRfc(link.getLinkedNode(), userId);
+        }
     }
-  }
 
-  /**
-   * Upsert collection rfc.
-   *
-   * @param rfcNodeList the rfc node list
-   * @param userId the user id
-   */
-  public void upsertCollectionRfc(List<CollectionRfcNode> rfcNodeList, String userId) {
-    for (CollectionRfcNode rfcNode : rfcNodeList) {
-      upsertCollectionRfc(rfcNode, userId);
+    /**
+     * Upsert collection rfc.
+     *
+     * @param rfcNodeList the rfc node list
+     * @param userId the user id
+     */
+    public void upsertCollectionRfc(List<CollectionRfcNode> rfcNodeList, String userId) {
+        for(CollectionRfcNode rfcNode: rfcNodeList) {
+            upsertCollectionRfc(rfcNode, userId);
+        }
     }
-  }
 
-  /**
-   * Upsert collection ci.
-   *
-   * @param ciNode the ci node
-   */
-  public void upsertCollectionCi(CollectionNode ciNode) {
-    CmsCI ci = cmProcessor.upsertCmsCI(ciNode);
+    /**
+     * Upsert collection ci.
+     *
+     * @param ciNode the ci node
+     */
+    public void upsertCollectionCi(CollectionNode ciNode) {
+        CmsCI ci = cmProcessor.upsertCmsCI(ciNode);
 
-    for (CollectionLink link : ciNode.getRelations()) {
-      link.setFromCiId(ci.getCiId());
-      cmProcessor.upsertRelation(link);
-      upsertCollectionCi(link.getLinkedNode());
+        for(CollectionLink link: ciNode.getRelations()) {
+        	link.setFromCiId(ci.getCiId());
+            cmProcessor.upsertRelation(link);
+            upsertCollectionCi(link.getLinkedNode());
+        }
     }
-  }
 
-  /**
-   * Upsert collection ci.
-   *
-   * @param ciNodeList the ci node list
-   */
-  public void upsertCollectionCi(List<CollectionNode> ciNodeList) {
-    for (CollectionNode ciNode : ciNodeList) {
-      upsertCollectionCi(ciNode);
+    /**
+     * Upsert collection ci.
+     *
+     * @param ciNodeList the ci node list
+     */
+    public void upsertCollectionCi(List<CollectionNode> ciNodeList) {
+        for(CollectionNode ciNode: ciNodeList) {
+            upsertCollectionCi(ciNode);
+        }
     }
-  }
 
 }
