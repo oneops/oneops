c67be2c8871fbd26ce49253fb498ed552d64ac95
Phat Huynh <sleepyorion@gmail.com>
2017-10-19 07:15:06 -0700

    install initial vagrant base image is not install

--- oneops-packer/build.sh
+++ oneops-packer/build.sh
@@ -16,6 +16,10 @@ fi
 
 if [ ! -f $BASE_IMAGE_FILE ]
 then
+  if [ ! -f "${HOME}/.vagrant.d/boxes/box-cutter-VAGRANTSLASH-centos73/2.0.21/virtualbox/box.ovf" ]
+  then
+    vagrant box add box-cutter/centos73 --box-version 2.0.21 --provider virtualbox
+  fi
   packer build centos-oneops-base.json
 fi
 
