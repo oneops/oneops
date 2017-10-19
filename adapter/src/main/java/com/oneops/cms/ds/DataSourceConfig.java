db62be6036acfd90856e8181148051c30cbd9c82
bannama <bhaskar.annamalai@gmail.com>
2017-10-19 16:58:15 -0700

    support for query secondary database based on request header

--- adapter/src/main/java/com/oneops/cms/ds/DataSourceConfig.java
+++ adapter/src/main/java/com/oneops/cms/ds/DataSourceConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
@@ -13,6 +14,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@EnableAspectJAutoProxy
 public class DataSourceConfig {
 
     @Value("${CMS_DB_USER}")
