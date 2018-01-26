6c53401c5d59546de6d158ae33cb1ef5b859ed55
Gaurav Lall <glall@walmartlabs.com>
2018-01-26 12:07:43 -0800

    Fix build

--- transistor/src/main/java/com/oneops/transistor/service/FlexManagerImpl.java
+++ transistor/src/main/java/com/oneops/transistor/service/FlexManagerImpl.java
@@ -111,6 +111,10 @@ public class FlexManagerImpl implements FlexManager {
 			CmsCIRelationAttribute adminstatus = cloudRel.getAttribute("adminstatus");
 			if (adminstatus == null || CmsConstants.CLOUD_STATE_ACTIVE.equals(adminstatus.getDjValue())) {
 				bomGenerationProcessor.processManifestPlatform(context,
+															   context.loadPlatformContext(platform),
+															   cloudRel,
+															   1,
+															   false);
 			}
 		}
 		
