d88936563bfd4d8f888b5553dd9d710c386a8be2
Oleg Kornev <okornev@users.noreply.github.com>
2018-03-08 23:43:06 -0500

    added tests to make sure getOpenRfcByCidIdList call is supported by InMemoryDJMapper (#406)
    log exception in case of design pull failure

---
+++ transistor/src/test/java/com/oneops/transistor/service/peristenceless/CmsRfcProcessorTest.java
@@ -0,0 +1,42 @@
+package com.oneops.transistor.service.peristenceless;
+
+import com.oneops.cms.dj.domain.CmsRfcCI;
+import com.oneops.cms.dj.service.CmsRfcProcessor;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.testng.Assert.*;
+
+/*******************************************************************************
+ *
+ *   Copyright 2016 Walmart, Inc.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ *******************************************************************************/
+public class CmsRfcProcessorTest {
+
+    /**
+     * Making sure InMemory DJMapper doesn't fail getOenRfcCIByCiIdList CmsRfcProcessor call
+     */
+    @Test
+    public void testGetOpenRfcCIByCiIdList(){
+        CmsRfcProcessor processor = new CmsRfcProcessor();
+        processor.setDjMapper(new InMemoryDJMapper());
+        ArrayList<Long> ids = new ArrayList<>();
+        ids.add(1L);
+        assertEquals(0, processor.getOpenRfcCIByCiIdList(ids).size());
+    }
+}
