bf72a16dbd396283aa1ee1325af20d1a5011e386
msumankumar <m.suman27@gmail.com>
2018-04-03 16:03:56 -0700

    Forcing to use init instead of systemd for nagios service

--- oneops-admin/lib/shared/cookbooks/monitor/recipes/add.rb
+++ oneops-admin/lib/shared/cookbooks/monitor/recipes/add.rb
@@ -453,6 +453,7 @@ else
      node.workorder.payLoad.Environment[0][:ciAttributes][:monitoring] == 'true'
 
     service nagios_service do
+      provider Chef::Provider::Service::Redhat if node[:platform_family].include?("rhel")
       supports [ :restart, :enable ]
       action [ :restart, :enable ]
     end
