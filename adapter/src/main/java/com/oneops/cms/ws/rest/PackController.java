710ecf679c790e8b77375d185f0ba5770045e9c9
bannama <bhaskar.annamalai@gmail.com>
2018-08-03 12:00:05 -0700

    update services attribute if required update services attribute if required update services attribute if required update services attribute if required

--- adapter/src/main/java/com/oneops/cms/ws/rest/PackController.java
+++ adapter/src/main/java/com/oneops/cms/ws/rest/PackController.java
@@ -6,9 +6,11 @@ import static org.springframework.beans.BeanUtils.copyProperties;
 import com.oneops.cms.cm.domain.CmsCI;
 import com.oneops.cms.cm.domain.CmsCIAttribute;
 import com.oneops.cms.cm.domain.CmsCIRelation;
+import com.oneops.cms.cm.domain.CmsCIRelationAttribute;
 import com.oneops.cms.cm.service.CmsCmProcessor;
 import com.oneops.cms.exceptions.CmsException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -41,18 +43,28 @@ public class PackController extends AbstractRestController {
 
   @RequestMapping(value = "/packs/sync/gslb-migrate", method = RequestMethod.POST)
   @ResponseBody
-  public Set<String> syncGslbMigrationProcedure(@RequestParam String source) {
+  public Set<String> syncGslbMigrationProcedure(
+      @RequestParam String source, @RequestParam(required = false) String pack) {
+    if (pack == null) {
+      pack = "";
+    }
     String packBaseNsPath = "/public/" + source + "/packs/";
-    List<CmsCIRelation> procedureRelations = getAllGslbMigrationProcedures(packBaseNsPath);
-    if (procedureRelations == null || procedureRelations.isEmpty()) {
+    List<CmsCIRelation> procedureRelations = getAllPlatformProcedures(packBaseNsPath);
+    procedureRelations =
+        procedureRelations
+            .stream()
+            .filter(r -> "gslb-migration".equals(r.getToCi().getCiName()))
+            .collect(Collectors.toList());
+    if (procedureRelations.isEmpty()) {
       throw new CmsException(
           CMS_EXCEPTION, "gslb-migration procedure ControlledBy relations not found");
     }
     Set<String> existingNsPaths =
         procedureRelations.stream().map(CmsCIRelation::getNsPath).collect(Collectors.toSet());
-    logger.info("gslb-migration available in these nsPaths " + existingNsPaths);
+    logger.info("gslb-migration procedure available in these nsPaths " + existingNsPaths);
 
-    CmsCI existingPlatformCi = procedureRelations.get(0).getFromCi();
+    CmsCIRelation existingRelation = findProcedureRelationForRedundant(procedureRelations);
+    CmsCI existingPlatformCi = existingRelation.getFromCi();
     CmsCIRelation templateProcRelation = templateRelation(procedureRelations.get(0));
     CmsCIRelation fqdnPayloadRelation = templateRelation(getFqdnPayload(existingPlatformCi));
 
@@ -60,15 +72,16 @@ public class PackController extends AbstractRestController {
 
     // for catalog ns we need to add only the procedure, for manifest ns we need to add the
     // procedure and the payload
+    String packsNsPath = packBaseNsPath + pack;
     List<CmsCI> catalogPlatforms =
-        cmProcessor.getCiBy3NsLike(packBaseNsPath, MGMT_CATALOG_PLATFORM_CLASS, null);
+        cmProcessor.getCiBy3NsLike(packsNsPath, MGMT_CATALOG_PLATFORM_CLASS, null);
     List<String> addedNsPaths =
         addRelationToNsPath(
             catalogPlatforms, templateProcRelation, Optional.empty(), existingNsPaths);
     nsPaths.addAll(addedNsPaths);
 
     List<CmsCI> manifestPlatforms =
-        cmProcessor.getCiBy3NsLike(packBaseNsPath, MGMT_MANIFEST_PLATFORM_CLASS, null);
+        cmProcessor.getCiBy3NsLike(packsNsPath, MGMT_MANIFEST_PLATFORM_CLASS, null);
     addedNsPaths =
         addRelationToNsPath(
             manifestPlatforms,
@@ -79,14 +92,27 @@ public class PackController extends AbstractRestController {
     return nsPaths;
   }
 
+  private CmsCIRelation findProcedureRelationForRedundant(List<CmsCIRelation> relations) {
+    Optional<CmsCIRelation> relationOpt =
+        relations.stream().filter(r -> r.getNsPath().endsWith("/redundant")).findFirst();
+    if (relationOpt.isPresent()) return relationOpt.get();
+    throw new CmsException(
+        CMS_EXCEPTION, "Existing procedure relation for redundant nsPath not found");
+  }
+
+  /**
+   * creates the gslb-migration procedure in the pack nsPath, creates the fqdnclouds payload (for
+   * fqdn component) required for migration/torbit deployments updates the services attribute of
+   * requires relation (from:platform to:fqdn) to include *torbit if needed
+   */
   private List<String> addRelationToNsPath(
       List<CmsCI> platforms,
       CmsCIRelation templateProcedureRelation,
       Optional<CmsCIRelation> templateFqdnPayloadRelation,
       Set<String> existingNsPaths) {
 
-    // we only need to create the cm_ci_relation record, ci record for toCi, fromCi should already
-    // be present
+    // we only need to create the cm_ci_relation record, ci record for toCi.
+    // fromCi should already be present
     List<String> nsPaths = new ArrayList<>();
     List<CmsCI> platformsToBeAdded =
         platforms
@@ -99,27 +125,57 @@ public class PackController extends AbstractRestController {
 
     for (CmsCI platform : platformsToBeAdded) {
       String nsPath = platform.getNsPath();
-      Optional<CmsCI> fqdnOpt = getFqdnCI(nsPath);
-      if (fqdnOpt.isPresent()) {
-        logger.info("gslb-migration adding the procedure to nsPath : " + nsPath);
+      Optional<CmsCIRelation> fqdnRelnOpt = getFqdnRequireRelation(platform);
+      if (fqdnRelnOpt.isPresent()) {
+        logger.info("gslb-migration-sync adding the procedure to nsPath : " + nsPath);
         CmsCIRelation newProcedureRelation =
             copy(templateProcedureRelation, nsPath, platform.getCiId());
         cmProcessor.createRelation(newProcedureRelation);
 
+        CmsCIRelation fqdnRequiresRelation = fqdnRelnOpt.get();
+
         // add fqdnclouds payload if not already present
         if (templateFqdnPayloadRelation.isPresent() && !isFqdnPayloadAvailable(nsPath)) {
-          logger.info("gslb-migration adding the fqdnclouds payload to nsPath : " + nsPath);
+          logger.info("gslb-migration-sync adding the fqdnclouds payload to nsPath : " + nsPath);
           CmsCIRelation newFqdnPayloadRelation =
-              copy(templateFqdnPayloadRelation.get(), nsPath, fqdnOpt.get().getCiId());
+              copy(
+                  templateFqdnPayloadRelation.get(),
+                  nsPath,
+                  fqdnRequiresRelation.getToCi().getCiId());
           cmProcessor.createRelation(newFqdnPayloadRelation);
         }
+
+        // add torbit to services attribute of Requires relation if needed
+        CmsCIRelationAttribute servicesAttr = fqdnRequiresRelation.getAttribute("services");
+        if (servicesAttr != null) {
+          if (updateServicesAttrIfNeeded(servicesAttr)) {
+            logger.info("gslb-migration-sync updating services attribute for : " + nsPath);
+            cmProcessor.updateRelation(fqdnRequiresRelation);
+          }
+        }
+
         nsPaths.add(nsPath);
       } else {
         logger.info(
-            "gslb-migration no fqdn ci present in nsPath, not adding procedure, payload " + nsPath);
+            "gslb-migration-sync no fqdn ci present in nsPath, not adding procedure, payload " + nsPath);
       }
     }
     return nsPaths;
+  }
+
+  private boolean updateServicesAttrIfNeeded(CmsCIRelationAttribute servicesAttr) {
+    boolean isUpdated = false;
+    String dfValue = servicesAttr.getDfValue();
+    if (!dfValue.contains("torbit")) {
+      servicesAttr.setDfValue(dfValue + ",*torbit");
+      isUpdated = true;
+    }
+    String djValue = servicesAttr.getDjValue();
+    if (!djValue.contains("torbit")) {
+      servicesAttr.setDjValue(djValue + ",*torbit");
+      isUpdated = true;
+    }
+    return isUpdated;
   }
 
   private boolean isFqdnPayloadAvailable(String nsPath) {
@@ -127,16 +183,23 @@ public class PackController extends AbstractRestController {
     return cis != null && !cis.isEmpty();
   }
 
-  private Optional<CmsCI> getFqdnCI(String nsPath) {
-    List<CmsCI> cis = cmProcessor.getCiBy3(nsPath, "Fqdn", "fqdn");
-    if (cis != null && !cis.isEmpty()) {
-      return Optional.of(cis.get(0));
+  private Optional<CmsCIRelation> getFqdnRequireRelation(CmsCI platform) {
+    List<CmsCIRelation> requiresRelations =
+        cmProcessor.getFromCIRelations(platform.getCiId(), "mgmt.Requires", null, "Fqdn");
+    if (requiresRelations != null) {
+      Optional<CmsCIRelation> requiresRelnOpt =
+          requiresRelations
+              .stream()
+              .filter(r -> "fqdn".equals(r.getToCi().getCiName()))
+              .findFirst();
+      return requiresRelnOpt;
     }
     return Optional.empty();
   }
 
   private CmsCIRelation getFqdnPayload(CmsCI platform) {
     logger.info("getting existing fqdnclouds payload from nsPath " + platform.getNsPath());
+    //payload relation mgmt.manifest.Payload from:fqdn to:payload
     List<CmsCIRelation> payloadRelations =
         cmProcessor.getCIRelations(
             platform.getNsPath(), MGMT_MANIFEST_PAYLOAD_RELATION, null, "Fqdn", null);
@@ -180,15 +243,18 @@ public class PackController extends AbstractRestController {
             && ("single".equals(nsElements[6]) || "redundant".equals(nsElements[6])));
   }
 
-  private List<CmsCIRelation> getAllGslbMigrationProcedures(String packNsPath) {
-    return cmProcessor.getCIRelationsNsLikeNakedNoAttrs(
-        packNsPath,
-        MGMT_CONTROLLER_BY_RELATION,
-        null,
-        "Platform",
-        MGMT_PROCEDURE_CLASS,
-        true,
-        true);
+  private List<CmsCIRelation> getAllPlatformProcedures(String packNsPath) {
+    //platform procedure relation mgmt.manifest.ControlledBy (from:platform  to:procedure)
+    List<CmsCIRelation> list =
+        cmProcessor.getCIRelationsNsLikeNakedNoAttrs(
+            packNsPath,
+            MGMT_CONTROLLER_BY_RELATION,
+            null,
+            "Platform",
+            MGMT_PROCEDURE_CLASS,
+            true,
+            true);
+    return list != null ? list : Collections.emptyList();
   }
 
   private CmsCIRelation templateRelation(CmsCIRelation procedureRelation) {
