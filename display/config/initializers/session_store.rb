86185de8cd73243bc9e70c37179becef48d4688f
Suresh G <sureshg.g@gmail.com>
2020-08-12 09:52:13 -0700

    misc changes

--- display/config/initializers/session_store.rb
+++
@@ -1,5 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Display::Application.config.session_store :cookie_store,
-                                          :key    => '_oneops-display_session',
-                                          :secure => !(Rails.env.development? || Rails.env.shared?)
